07b70790318beb27fc52547be1c4de0eede3b329
Jan Maxa <hrakuun@gmail.com>
2024-01-23 15:33:42 +0100

    úkoly hotovy

--- src/Main.java
+++ src/Main.java
@@ -31,26 +31,26 @@ public class Main {
         System.out.println(cena);
         // Má vypsat "1.0". Ale vypíše "0.9999" !!!
     }
-//
-//    public static void ukol6() {
-//        BigDecimal cena = 0;
-//        cena = cena.add(BigDecimal.valueOf(0.1));
-//        System.out.println(cena);
-//    }
-//
-//    public static void ukol7() {
-//        int vykonMotoru = "120";
-//        System.out.println("Výkon Motoru je: "+vykonMotoru+" kW.");
-//    }
-//
-//    public static void ukol8() {
-//        int velikostKosile = 37;
-//        System.out.println("Velikost košile je: ", velikostKosile, ".");
-//    }
-//
-//    public void ukol9() {
-//        System.out.println("Metoda *skoro* bez chybičky!");
-//    }
+
+    public static void ukol6() {
+        BigDecimal cena = BigDecimal.valueOf(0);
+        cena = cena.add(BigDecimal.valueOf(0.1));
+        System.out.println(cena);
+    }
+
+    public static void ukol7() {
+        int vykonMotoru = 120;
+        System.out.println("Výkon Motoru je: " + vykonMotoru + " kW.");
+    }
+
+    public static void ukol8() {
+        int velikostKosile = 37;
+        System.out.println("Velikost košile je: " + velikostKosile + ".");
+    }
+
+    public static void ukol9() {
+        System.out.println("Metoda *skoro* bez chybičky!");
+    }
 
     public static void main(String[] args) {
         System.out.println("Oprav chyby a spusť všechny metody!");
@@ -62,9 +62,9 @@ public class Main {
         ukol3();
         ukol4();
         ukol5();
-//        ukol6();
-//        ukol7();
-//        ukol8();
-//        ukol9();
+        ukol6();
+        ukol7();
+        ukol8();
+        ukol9();
     }
 }
